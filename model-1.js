2b7fe7fa76144ae97ea291a831133eaabce71200
Alexis Newby <alexis.newby@gmail.com>
2021-09-16 20:15:34 -0600

    Add number variables

--- model-1.js
+++ model-1.js
@@ -1,3 +1,9 @@
+//Strings
 var waterBottle = "Hydroflask"
 var waterBottleColor = "white"
 var waterBottleTop = "straw"
+
+//Numbers
+var numberOfHikingTrailStickers = 4
+var numberOfSchittsCreekStickers = 1
+var numberOfColoradoStickers = 3
